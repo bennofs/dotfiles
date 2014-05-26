d6eaf9e0d29769ac00abffdef4d95f8a7ba68fc6
Benno Fünfstück <benno.fuenfstueck@gmail.com>
2014-05-26 20:25:52 +0200

    Add solarized-light to safe themes

--- .emacs.d/config/config-theme.el
+++ .emacs.d/config/config-theme.el
@@ -1,4 +1,4 @@
-(setq custom-safe-themes '("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default))
+(setq custom-safe-themes '("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default))
 (setq ansi-color-names-vector ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
 (load-theme 'solarized-dark)
 
