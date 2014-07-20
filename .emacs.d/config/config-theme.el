c2f615c6a03f5b7a5104420d162d9c7114a54707
Benno Fünfstück <benno.fuenfstueck@gmail.com>
2014-07-20 12:16:00 +0200

    Switch to dark color again

--- .emacs.d/config/config-theme.el
+++ .emacs.d/config/config-theme.el
@@ -4,6 +4,6 @@
   "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" ; solarized-dark
   default))
 (setq ansi-color-names-vector ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
-(load-theme 'solarized-light)
+(load-theme 'solarized-dark)
 
 (provide 'config-theme)
