8f6e6803b90f465e9f8ef17ec9616acb7221244e
Benno Fünfstück <benno.fuenfstueck@gmail.com>
2014-07-20 12:23:29 +0200

    emacs: restore darker background

--- .emacs.d/config/config-faces.el
+++ .emacs.d/config/config-faces.el
@@ -10,6 +10,12 @@
   :height 90
   :width 'normal
   :font "Source Code Pro for Powerline"
+  :background "#000e16"
 )
+
+(set-face-attribute 'fringe nil :background "#000e16")
+(set-face-attribute 'linum  nil :background "#000e16")
+(set-face-attribute 'mode-line-inactive nil :background "#000e16" :underline nil)
+(set-face-attribute 'mode-line nil :background "#001e30" :underline nil)
 
 (provide 'config-faces)
