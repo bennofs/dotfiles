d354303df3778bb51d1bd35034faac0b8d64fb55
Benno Fünfstück <benno.fuenfstueck@gmail.com>
2014-07-30 20:17:13 +0200

    add fish config

---
+++ .config/fish/config.fish
@@ -0,0 +1,37 @@
+# fish git prompt
+set __fish_git_prompt_showdirtystate 'yes'
+set __fish_git_prompt_showstashstate 'yes'
+set __fish_git_prompt_showupstream 'yes'
+set __fish_git_prompt_color_branch yellow
+
+# Status Chars
+set __fish_git_prompt_char_dirtystate '⚡'
+set __fish_git_prompt_char_stagedstate '→'
+set __fish_git_prompt_char_stashstate '↩'
+set __fish_git_prompt_char_upstream_ahead '↑'
+set __fish_git_prompt_char_upstream_behind '↓'
+
+function fish_prompt
+  set last_status $status
+  set_color $fish_color_cwd
+  printf '%s' (prompt_pwd)
+  set_color normal
+  printf '%s ' (__fish_git_prompt)
+  set_color normal
+end
+
+# fish calls command-not-found with an additional argument,
+# which is not supported by NixOS command-not-found script.
+# This function strips that argument.
+function command-not-found
+  /run/current-system/sw/bin/command-not-found $argv[2]
+end
+
+eval (hub alias -s)
+
+# git aliases
+alias gst="git status -sb"
+alias gp="git pull"
+alias gup="git push"
+alias glo="git log --oneline --pretty --first-parent"
+alias gd="git diff"
