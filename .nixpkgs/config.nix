90b1bc90634476efdf36f1f3aa6639049090d08a
Benno Fünfstück <benno.fuenfstueck@gmail.com>
2015-08-22 16:43:13 +0200

    cleanup

--- .nixpkgs/config.nix
+++ .nixpkgs/config.nix
@@ -1,4 +1,4 @@
-with (import <nixpkgs/lib>); let
+with (import <nixpkgs/lib>); with builtins; let
   inherit (builtins) getEnv;
   ignoredDirectories = [".git" ".hg" "build" "dist"];
   preservedEnvvars = [
@@ -14,7 +14,7 @@ with (import <nixpkgs/lib>); let
   ];
   devPkgs = pkgs: with pkgs; [
     less git mercurial fish gitAndTools.hub utillinux bc man manpages
-    nano openssh haskellPackages.cabal-bounds neovim
+    nano openssh haskellPackages.cabal-bounds vimHugeX nix
   ];
   setupEnv = concatStringsSep "\n" (map (x: "export ${x}=${getEnv x}") preservedEnvvars);
   localSourceFilter = path: type:
@@ -27,44 +27,33 @@ with (import <nixpkgs/lib>); let
   systemConf = import /etc/nixos/conf/nixpkgs.nix;
 in systemConf // {
   allowBroken = true;
+
   haskellPackageOverrides = self: super: {
     localPackage = s: self.callPackage (import s {}).expr;
   };
+
   packageOverrides = pkgs: systemConf.packageOverrides pkgs // rec {
     stdenv = pkgs.stdenv // rec {
       mkDerivation = args:
         let
-          shellHook = old: ''
-            ${old.shellHook or ""}
-            ${setupEnv}
-          '';
-          noLocalSrc = !(args ? src) || pkgs.lib.isDerivation args.src;
-          newArgs =
-            if noLocalSrc then args else args // {
-              src =
-                if hasPrefix builtins.storeDir (builtins.toString args.src)
-                  then args.src
-                  else builtins.filterSource localSourceFilter args.src;
-              passthru = (args.passthru or {}) // {
-                oldEnv = args.passthru.env;
-                env = let oldEnv = args.passthru.env or result; in
-                 pkgs.lib.overrideDerivation oldEnv (envArgs: {
-                  buildInputs = devPkgs pkgs ++ oldEnv.buildInputs or [];
-                  src = null;
-                  shellHook = shellHook oldEnv;
-                 }
-                 ) // {
-                   build = result;
-                   override = f: (result.override f).build;
-                   env = result.env;
-                 };
-              };
-            };
-          result = pkgs.stdenv.mkDerivation newArgs;
+          localSrc = (args ? src) && !isDerivation args.src;
+          extraArgs = optionalAttrs localSrc localOverrides;
+          filterLocal = x: if isStorePath x then x else filterSource localSourceFilter x;
+          baseEnv = args.passthru.env or args.env or result;
+          localOverrides = { 
+            src = filterLocal args.src;
+            passthru.env = overrideDerivation baseEnv (baseArgs: {
+              nativeBuildInputs = devPkgs pkgs ++ baseArgs.nativeBuildInputs or [];
+              src = null;
+              shellHook = ''
+                ${baseArgs.shellHook or ""}
+                ${setupEnv}
+              '';
+            });
+          };
+          result = pkgs.stdenv.mkDerivation (recursiveUpdate args extraArgs);
         in result;
     };
-    haskellPackages = pkgs.haskellPackages.override (old: { pkgs = old.pkgs // { inherit stdenv; }; });
-    haskellngPackages = haskellPackages;
-  };
+  }; 
 }
 
--- .themplate/haskell-nix/default.nix
+++ .themplate/haskell-nix/default.nix
@@ -3,8 +3,9 @@ let
   nativePkgs = import <nixpkgs> {};
   expr = nativePkgs.stdenv.mkDerivation ({
     name = "project.nix";
+    src = ./.;
     buildCommand = ''
-      ${nativePkgs.haskellngPackages.cabal2nix}/bin/cabal2nix ${./.} > $out
+      ${nativePkgs.haskellngPackages.cabal2nix}/bin/cabal2nix $src > $out
     '';
   } // nativePkgs.lib.optionalAttrs nativePkgs.stdenv.isLinux {
     LANG = "en_US.UTF-8";
