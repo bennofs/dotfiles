e0a49d2cd5d83fe48e075dbca13b168c927ed52d
Benno Fünfstück <benno.fuenfstueck@gmail.com>
2015-10-11 15:06:10 +0200

    add missing files

---
+++ .config/systemd/units.nix
@@ -0,0 +1,36 @@
+with (import <nixpkgs> {});
+let
+  mpdDataDir = "/home/.local/share/mpd";
+  mpdConf = pkgs.writeText "mpd.conf" ''
+    music_directory     "/data/music"
+    playlist_directory  "${mpdDataDir}/playlists"
+    db_file             "${mpdDataDir}/tag_cache"
+    state_file          "${mpdDataDir}/state"
+    sticker_file        "${mpdDataDir}/sticker.sql"
+    log_file            "syslog"
+    log_level           "default"
+    audio_output {
+      type "alsa"
+      name "default"
+    }
+  '';
+in {
+
+gdrive = ''
+ [Unit]
+ Description=Google Drive Synchronization Daemon
+ [Service]
+ ExecStart=${google-drive-ocamlfuse}/bin/google-drive-ocamlfuse -f /home/gdrive
+ Environment=PATH=/var/setuid-wrappers
+'';
+
+mpd = ''
+ [Unit]
+ Description="Music Player Dameon";
+ #Requires=pulseaudio.service
+ [Service]
+ ExecStartPre=${coreutils}/bin/mkdir -p ${mpdDataDir}
+ ExecStart=${mpd}/bin/mpd --no-daemon ${mpdConf}
+'';
+
+}
