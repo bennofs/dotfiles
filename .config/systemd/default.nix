fc3fe1f72b5f5b6abe4c2e500d3f9c3a9ce39d28
Benno Fünfstück <benno.fuenfstueck@gmail.com>
2015-11-04 22:39:33 +0100

    add systemd unit default.nix

---
+++ .config/systemd/default.nix
@@ -0,0 +1,16 @@
+with (import <nixpkgs> {}); with lib; with builtins;
+
+let
+  units = import ./units.nix;
+  mkName = x: if length (splitString "." x) == 1 then x + ".service" else x;
+  makeUnit = name: unit: '' echo "${unit}" > "${mkName name}"'';
+  linkUnit = name: _   : '' ln -s $out/${mkName name} $out/default.target.wants/${mkName name} '';
+in stdenv.mkDerivation {
+  name = "services";
+  buildCommand = ''
+    mkdir -p $out/default.target.wants
+    cd $out
+    ${lib.concatStringsSep "\n" (lib.mapAttrsToList makeUnit units)};
+    ${lib.concatStringsSep "\n" (lib.mapAttrsToList linkUnit units)};
+  '';
+}
