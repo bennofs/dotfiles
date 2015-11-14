1c9a369022847a230959679f69014ef2840e18f1
Benno Fünfstück <benno.fuenfstueck@gmail.com>
2015-11-14 21:18:06 +0100

    update haskell nix template to non-ng

--- .themplate/haskell-nix/default.nix
+++ .themplate/haskell-nix/default.nix
@@ -1,17 +1,18 @@
-{ haskellngPackages ? (import <nixpkgs> {}).haskellngPackages }:
+{ haskellPackages ? (import <nixpkgs> {}).haskellPackages }:
 let
   nativePkgs = import <nixpkgs> {};
   expr = nativePkgs.stdenv.mkDerivation ({
     name = "project.nix";
     src = ./.;
     buildCommand = ''
-      ${nativePkgs.haskellngPackages.cabal2nix}/bin/cabal2nix $src > $out
+      ${nativePkgs.haskellPackages.cabal2nix}/bin/cabal2nix $src > $out
     '';
   } // nativePkgs.lib.optionalAttrs nativePkgs.stdenv.isLinux {
     LANG = "en_US.UTF-8";
     LOCALE_ARCHIVE = "${nativePkgs.glibcLocales}/lib/locale/locale-archive";
   });
-  hs = haskellngPackages.override (old: {
+  inherit (haskellPackages) lib;
+  hs = haskellPackages.override (old: {
     overrides = self: oldsuper: let super = oldsuper // (old.overrides or (_:_:{})) self oldsuper; in super // {
     };
   });
