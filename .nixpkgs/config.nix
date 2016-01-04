653c0a443a2e2ab06b80ec7d4060eeb0e7c95356
Benno Fünfstück <benno.fuenfstueck@gmail.com>
2016-01-04 20:54:57 +0100

    small fixes

--- .nixpkgs/config.nix
+++ .nixpkgs/config.nix
@@ -16,7 +16,7 @@ with (import <nixpkgs/lib>); with builtins; let
     less git mercurial fish gitAndTools.hub utillinux bc man manpages
     nano openssh haskellPackages.cabal-bounds vimHugeX nix haskellPackages.ghc-mod
     haskellPackages.cabal-install patchutils haskellPackages.hscolour perl time linuxPackages.perf
-    gdb
+    gdb utillinuxCurses
   ];
   setupEnv = concatStringsSep "\n" (map (x: "export ${x}=${getEnv x}") preservedEnvvars);
   localSourceFilter = path: type:
