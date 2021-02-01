bcd4de920b1aec516efdbe6fad504524fcb758c4
Benno Fünfstück <benno.fuenfstueck@gmail.com>
2021-02-01 22:38:30 +0100

    more updates

--- .doom.d/packages.el
+++ .doom.d/packages.el
@@ -12,7 +12,7 @@
 (package! clean-aindent-mode :pin "a97bcae8f43a9ff64e95473e4ef0d8bafe829211")
 (package! promela-mode :recipe (:host github :repo "rudi/promela-mode") :pin "5974a15221720fdb4eb825da01282289badc4884")
 (package! org-present :recipe (:host github :repo "rlister/org-present") :pin "9709ca2d04a59959354222ac4d3f8b750785739a")
-(package! centered-window-mode :recipe (:host github :repo "anler/centered-window-mode") :pin "f50859941ab5c7cbeaee410f2d38716252b552ac")
+;(package! centered-window-mode :recipe (:host github :repo "anler/centered-window-mode") :pin "f50859941ab5c7cbeaee410f2d38716252b552ac")
 (package! hide-lines
   :recipe
   (:host github
